59b0925f6bf6c0ce503a45ef1f11cf899897949a
okadahideya <73088476+okadahideya@users.noreply.github.com>
2020-11-26 18:31:29 +0900

    教材管理ページ　HTML、CSS

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,4 +1,13 @@
 class SubjectsController < ApplicationController
   def index
   end
+
+  def new
+    if user_signed_in?
+      @subject = Subject.new
+    else
+      render :index
+    end
+  end
+
 end
--- app/models/result_genre.rb
+++ app/models/result_genre.rb
@@ -2,7 +2,8 @@ class ResultGenre < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '取得' },
-    { id: 3, name: '落第' }
+    { id: 3, name: '落第' },
+    { id: 4, name: '受講中' }
   ]
   include ActiveHash::Associations
   has_many :subjects
